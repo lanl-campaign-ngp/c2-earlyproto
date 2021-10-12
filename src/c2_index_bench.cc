6ae88ff1ad653ae7fbcdff0e7af9b30a2a51990f
Qing Zheng <qzheng@newmexicoconsortium.org>
2021-10-12 15:09:51 -0400

    add index bench

--- src/c2_index_bench.cc
+++ src/c2_index_bench.cc
@@ -31,7 +31,92 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "c2_env.h"
+#include "c2_index_builder.h"
+#include "c2_random.h"
+
+#include <fastbit/part.h>
+#include <stdio.h>
+#include <vector>
+
+static int FLAGS_n = 10000000;  // Number of keys
+
+static int FLAGS_value_min = 100;
+
+static int FLAGS_value_max = 1000;
+
+namespace c2 {
+
+class IndexBench {
+ public:
+  IndexBuilderOptions opts;
+  IndexBuilder* bu;
+  std::string tmpdir;
+
+  IndexBench() : bu(NULL) {
+    ibis::gVerbose = 5;
+    IO::Default()->GetTestDirectory(&tmpdir);
+    tmpdir += "/index_builder_test";
+    IO::Default()->CreateDir(tmpdir.c_str());
+    opts.ibis_col =
+        new ibis::column(new ibis::part(tmpdir.c_str(), NULL, false),
+                         ibis::FLOAT, "var0", "test_var");
+    bu = new IndexBuilder(opts, NULL);
+  }
+
+  ~IndexBench() {
+    delete opts.ibis_col->partition();
+    delete opts.ibis_col;
+    delete bu;
+  }
+
+  void Build(const std::vector<float>& inputdata) {
+    uint64_t begin = env::CurrentMicros();
+    bu->TEST_BuildIndexes(inputdata);
+    bu->print(std::cerr);
+    uint64_t d = env::CurrentMicros() - begin;
+    fprintf(stderr, "== Index built in %.3f s\n", double(d) / 1000.0 / 1000.0);
+  }
+
+  void LessThan(float a) {
+    ibis::bitvector results;
+    ibis::qContinuousRange r("var0", ibis::qExpr::OP_LT, a);
+    uint64_t begin = env::CurrentMicros();
+    bu->evaluate(r, results);
+    uint64_t d = env::CurrentMicros() - begin;
+    fprintf(stderr, "== Query evaluated in %.3f s\n",
+            double(d) / 1000.0 / 1000.0);
+  }
+};
+
+void BM_Main(int* const argc, char*** const argv) {
+  c2::IndexBench b;
+  c2::Random r(301);
+  std::vector<float> inputdata;
+  for (int i = 2; i < *argc; i++) {
+    int a;
+    char junk;
+    if (sscanf((*argv)[i], "--n=%d%c", &a, &junk) == 1 ||
+        sscanf((*argv)[i], "-n=%d%c", &a, &junk) == 1) {
+      FLAGS_n = a;
+    }
+  }
+  inputdata.reserve(FLAGS_n);
+  for (int i = 0; i < FLAGS_n; i++) {
+    inputdata.push_back(
+        float(FLAGS_value_min +
+            double(FLAGS_value_max - FLAGS_value_min) * r.Next() / INT32_MAX));
+  }
+  b.Build(inputdata);
+  for (int i = 1; i < 10; i++) {
+    b.LessThan(float(FLAGS_value_min +
+                     double(FLAGS_value_max - FLAGS_value_min) * i / 10));
+  }
+}
+
+}  // namespace c2
 
 int main(int argc, char* argv[]) {
+  c2::BM_Main(&argc, &argv);
   return 0;
 }
